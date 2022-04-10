2312e80f397a248de40d634591e08a53b903b4e0
Mi-Estancia <100036753+Mi-Estancia@users.noreply.github.com>
2022-04-10 13:03:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Se te notaba xd')
+    alert('Pasamela al wpp despues te pago en la escuela')
 });
 
 
