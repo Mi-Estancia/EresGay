278ca8fd19824f988a747b6e965b4e6181789540
Mi-Estancia <100036753+Mi-Estancia@users.noreply.github.com>
2022-04-10 13:00:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ yesBtn.addEventListener('click',function () {
     alert('Se te notaba xd')
 });
 
-const noBtn = document.querySelector('#noBtn');
 
-noBtn.addEventListener('mouseover', function () {
-    const randomX = parseInt(Math.random()*100);
-    const randomY = parseInt(Math.random()*100);
-    noBtn.style.setProperty('top',randomY+'%');
-    noBtn.style.setProperty('left',randomX+'%');
-    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
-})
+noBtn.addEventListener('click',function () {
+    alert('Que vago oe, ya deja las pajas')
+});
